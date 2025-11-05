263dc9f280593c9cbbf4295b6c5859249b92ee34
ajaikumarvs <ajaikumarvs@gmail.com>
2025-11-05 20:59:50 +0530

    add egui, basic ui setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("Claritty", native_options, Box::new(|cc| Ok(Box::new(Claritty::new(cc)))));
+}
+#[derive(Default)]
+struct Claritty {}
+
+impl Claritty {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for Claritty {
+   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Hello World!");
+       });
+   }
 }
